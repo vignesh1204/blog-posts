0eac45084df7f41cb8f9c69b197a7548786b9d57
Vignesh <vignesh1999@gmail.com>
2020-07-14 19:40:59 +0530

    wip3

---
+++ comments/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsById = {};
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsById[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsById[req.params.id] || [];
+    comments.push({ id: commentId, content});
+
+    commentsById[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log("Listening on 4001");
+});
